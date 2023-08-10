4a2eba834dfc854773399e7f9d69b8d11e4621a1
Alena <m1nima02@yandex.ru>
2023-08-10 09:48:55 +0300

    update

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ alert('Мир');
 let message = 'Hello!'; // определяем переменную и присваиваем ей значение
 
 alert(message); // Hello!
+
+<script language="JavaScript">
+ alert ("Привет, мир!");
+</script>
